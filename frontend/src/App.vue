c5f558aa8e38e1d61fa7274346a444fa2964d72e
Alan Fijal <alan.fijal@gmail.com>
2025-10-08 09:01:33 +0200

    Bug reporting added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,8 +8,22 @@
           <router-view />
         </v-container>
       </v-main>
- 
+
       <AppFooter />
+
+      <!-- Support Button -->
+      <v-btn
+        icon="mdi-bug-outline"
+        color="primary"
+        size="large"
+        position="fixed"
+        location="bottom right"
+        class="support-btn"
+        @click="openSupportEmail"
+      >
+        <v-icon>mdi-bug-outline</v-icon>
+        <v-tooltip activator="parent" location="left">Report a bug</v-tooltip>
+      </v-btn>
     </v-layout>
   </v-app>
 </template>
@@ -17,4 +31,18 @@
 <script setup>
 import AppNavbar from './components/layout/AppNavBar.vue';
 import AppFooter from './components/layout/AppFooter.vue';
+
+const openSupportEmail = () => {
+  const email = 'alanbruno.fijal@alumni.esade.edu'; 
+  const subject = 'Bug Report - Ennova Events Platform';
+  const body = 'Please describe the bug you encountered:\n\n';
+
+  window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+};
 </script>
+
+<style scoped>
+.support-btn {
+  margin: 0 16px 16px 0;
+}
+</style>
