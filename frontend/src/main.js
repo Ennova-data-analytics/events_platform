d6a917a15a9cdbed34c0de053599c8bfba017c77
Alan Fijal <alan.fijal@gmail.com>
2026-02-25 08:33:36 +0100

    Integrated google analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import vuetify from './plugins/vuetify'
+import { createGtag } from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -10,7 +11,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.use(vuetify)
+app.use(createGtag({
+  tagId: import.meta.env.VITE_GOOGLE_ANALYTICS_ID,
+  pageTracker: { router }
+}))
 
 app.mount('#app')
