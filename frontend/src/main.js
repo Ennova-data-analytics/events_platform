694357fb54082abfb49f1d883bb0fe7dc9350f96
Alan Fijal <alan.fijal@gmail.com>
2026-02-27 11:32:46 +0100

    Adjusted google analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import vuetify from './plugins/vuetify'
-import { createGtag } from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -12,9 +11,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
-app.use(createGtag({
-  tagId: import.meta.env.VITE_GOOGLE_ANALYTICS_ID,
-  pageTracker: { router }
-}))
 
 app.mount('#app')
